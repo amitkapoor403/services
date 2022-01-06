8fe77bcab2e6e0e58bad34056e25eef4961d90d0
amitkapoor403 <85727075+amitkapoor403@users.noreply.github.com>
2022-01-06 12:09:58 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-var navMenu = document.querySelector(".nav-list");
-
-hamburger.addEventListener("click", toggleButton);
-
-function toggleButton() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
